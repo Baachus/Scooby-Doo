36bd0efce8a2651c880cc2ef82795acaad57d487
Baachus <bc.robert@gmail.com>
2023-02-24 15:11:33 -0600

    Updated and cleaned up Run.sh

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -7,22 +7,22 @@ project="e2etest"
 
 cd "$(dirname "$0")/.."
 
-docker-compose -p "$project" build
+docker-compose -p "$project" -f path/to/docker-compose.yml build
 
+mkdir -m 777 -p "$(pwd)/Reports"
 
-mkdir -m 777 -p Reports
+docker-compose -p "$project" -f path/to/docker-compose.yml up -d scooby_api scooby_webapp scoob_db selenium-hub firefox chrome edge
+docker-compose -p "$project" -f path/to/docker-compose.yml up --no-deps scoob_test
 
-docker-compose -p "$project" up -d scooby_api scooby_webapp scoob_db selenium-hub firefox chrome edge
-docker-compose -p "$project" up --no-deps scoob_test
+container_id=$(docker ps -aqf "name=scoob_test")
+docker cp "$container_id:/src/ScoobTestBDD/LivingDoc.html" "$(pwd)/Reports"
+echo "SpecFlow LivingDoc report is copied to ./Reports"
+ls -l "$(pwd)/Reports"
 
-docker cp scoob_test:/src/ScoobTestBDD/LivingDoc.html ./Reports
-echo "Specflow living document report is copied to ./Reports"
-ls -l ./Reports
+exit_code=$(docker inspect "$container_id" -f '{{.State.ExitCode}}')
 
-exit_code=$(docker inspect scoob_test -f '{{.State.ExitCode}}')
-
-if [ $exit_code -eq 0 ]; then
-    exit $exit_code
+if [ "$exit_code" -eq 0 ]; then
+    exit "$exit_code"
 else
     echo "Test failed"
 fi
